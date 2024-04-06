d1d529292562e8529b69129524d5454daa405a54
auryouth <ausosa528@proton.me>
2024-04-06 16:14:04 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,7 +13,7 @@ return {
         sidebars = {
           "qf",
           "vista_kind",
-          "terminal",
+          -- "terminal",
           "spectre_panel",
           "startuptime",
           "Outline",
